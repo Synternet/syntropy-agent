7e9a1a88334e9387acb9c102c057e5f663e365da
Antanas Gadeikis <antanas@syntropynet.com>
2021-08-11 12:22:56 +0300

    Iptables implementation

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/containerd/containerd v1.5.4 // indirect
+	github.com/coreos/go-iptables v0.5.0
 	github.com/docker/docker v20.10.7+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/go-ping/ping v0.0.0-20210506233800-ff8be3320020
@@ -12,6 +13,7 @@ require (
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/vishvananda/netlink v1.1.1-0.20201029203352-d40f9887b852
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20210506160403-92e472f520a5
 	google.golang.org/grpc v1.39.0 // indirect
--- netfilter/netfilter.go
+++ netfilter/netfilter.go
@@ -2,32 +2,140 @@
 // and is used to setup Syntropy releated rules
 package netfilter
 
-import "log"
+import (
+	"log"
+
+	"github.com/coreos/go-iptables/iptables"
+	"github.com/vishvananda/netlink"
+)
 
 // TODO: review `-nft` and `-legacy` usage
 
+const (
+	defaultTable  = "filter"
+	natTable      = "nat"
+	syntropyChain = "SYNTROPY_CHAIN"
+)
+
 func CreateChain() error {
-	log.Println("iptables creating SYNTROPY_CHAIN")
+	rule := []string{"-s", "0.0.0.0/0", "-d", "0.0.0.0/0", "-j", syntropyChain}
+
+	ipt, err := iptables.New()
+	if err != nil {
+		return err
+	}
+
+	exists, err := ipt.ChainExists(defaultTable, syntropyChain)
+	if !exists && err == nil {
+		err = ipt.NewChain(defaultTable, syntropyChain)
+	}
+	if err != nil {
+		return err
+	}
+
+	exists, err = ipt.Exists(defaultTable, syntropyChain, rule...)
+	if !exists && err == nil {
+		err = ipt.Insert(defaultTable, syntropyChain, 1, rule...)
+	}
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
+func processPeerRule(ipt *iptables.IPTables, add bool, ip string) (err error) {
+	rule := []string{"-p", "all", "-s", ip, "-j", "ACCEPT"}
+	if add {
+		err = ipt.AppendUnique(defaultTable, syntropyChain, rule...)
+	} else {
+		err = ipt.DeleteIfExists(defaultTable, syntropyChain, rule...)
+	}
+	return err
+}
+
 func RulesAdd(ips ...string) error {
-	log.Println("iptables add ", ips)
+	ipt, err := iptables.New()
+	if err != nil {
+		return err
+	}
+	for _, ip := range ips {
+		err := processPeerRule(ipt, true, ip)
+		if err != nil {
+			log.Println(err)
+			// TODO fallback to last known good state ?
+			// TODO or maybe try adding all rules, and return concatenated errors ?
+			return err
+		}
+	}
 	return nil
 }
 
 func RulesDel(ips ...string) error {
-	log.Println("iptables del ", ips)
+	ipt, err := iptables.New()
+	if err != nil {
+		return err
+	}
+	for _, ip := range ips {
+		err := processPeerRule(ipt, false, ip)
+		if err != nil {
+			log.Println(err)
+			// TODO fallback to last known good state ?
+			// TODO or maybe try adding all rules, and return concatenated errors ?
+			return err
+		}
+	}
 	return nil
 }
 
 func ForwardEnable(ifname string) error {
-	log.Println("iptables MASQ ", ifname, defaultRouteIfname())
+	ipt, err := iptables.New()
+	if err != nil {
+		return err
+	}
+	forwardRule := []string{"-i", ifname, "-j", "ACCEPT"}
+	err = ipt.AppendUnique(defaultTable, "FORWARD", forwardRule...)
+	if err != nil {
+		return err
+	}
+
+	dri := defaultRouteIfname()
+	if dri != "" {
+		masquaradeRule := []string{"-o", defaultRouteIfname(), "-j", "MASQUERADE"}
+		err = ipt.AppendUnique(natTable, "POSTROUTING", masquaradeRule...)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func defaultRouteIfname() string {
 	var ifname string
+	var defaultRoute *netlink.Route
+	const AF_INET = 2 // from include/linux/socket.h. Is there a better way to use constant ?
+
+	routes, err := netlink.RouteList(nil, AF_INET)
+	if err != nil {
+		log.Println(err)
+		return ifname
+	}
+
+	for _, r := range routes {
+		if r.Dst == nil {
+			if defaultRoute == nil || defaultRoute.Priority > r.Priority {
+				defaultRoute = &r
+			}
+
+		}
+	}
+
+	if defaultRoute != nil {
+		l, err := netlink.LinkByIndex(defaultRoute.LinkIndex)
+		if err == nil && l != nil {
+			ifname = l.Attrs().Name
+		}
+	}
 
 	return ifname
 }
