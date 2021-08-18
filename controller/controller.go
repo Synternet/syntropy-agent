ef5be91da3db8746f5c358bdaf556a1298062add
Antanas Gadeikis <antanas@syntropynet.com>
2021-08-18 12:10:22 +0300

    Add comments explaining controller interface

--- controller/controller.go
+++ controller/controller.go
@@ -3,8 +3,14 @@ package controller
 import "io"
 
 type Controller interface {
-	// komentarai komentarai
-	io.Writer
-	io.Closer
+	// The primary idea was to use Reader interface here
+	// But the reader may read a partial message and this will complicate agent main loop
+	// and agent would be responsible for receiving and joining all message parts.
+	// So instead hide that joining part and make a controller responsible for receiving full message.
 	Recv() ([]byte, error)
+	// Writer method Write(b) is used for sending message to controller
+	io.Writer
+	// Close() terminates controller. After Close controller will not reconnect
+	// and may bot be used to  receive or send messages.
+	io.Closer
 }
