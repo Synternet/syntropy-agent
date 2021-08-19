fa36b2921fe852d5f24108185eba3310d591eafe
Antanas Gadeikis <antanas@syntropynet.com>
2021-08-19 11:19:01 +0300

    Script controller uses logger

--- controller/script/script.go
+++ controller/script/script.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"strings"
 	"time"
 
 	"github.com/SyntropyNet/syntropy-agent-go/config"
 	"github.com/SyntropyNet/syntropy-agent-go/controller"
+	"github.com/SyntropyNet/syntropy-agent-go/logger"
 )
 
 // Script controller just reads files from
@@ -44,7 +44,6 @@ func NewController() (controller.Controller, error) {
 	}
 	cc.stop = make(chan bool)
 
-	log.Println("Initialise Script controller. Scripts: ", cc.list, len(cc.list), ".")
 	return &cc, nil
 }
 
@@ -55,23 +54,28 @@ func (cc *ScriptController) Recv() ([]byte, error) {
 		fname := cc.list[cc.index]
 		cc.index++
 		if fname == "" || fname[0] == '#' {
+			logger.Debug().Printf("Script Controller: Skip \"%s\"\n", fname)
 			continue
 		}
 		msg, err := ioutil.ReadFile(scriptPath + "/" + fname)
 		if err != nil {
-			log.Printf("ScriptControler. File %s: %s", fname, err.Error())
+			logger.Error().Printf("ScriptControler: File %s: %s", fname, err.Error())
 			continue
 		}
+		logger.Debug().Printf("Script Controller: Receiving \"%s\"\n", fname)
 
 		return msg, nil
 	}
 
 	// When no more configuration scripts are left - just block the Recv
 	// and keep agent waiting
+	logger.Debug().Println("Script Controller: No more messages.")
 	_, ok := <-cc.stop
 	if !ok {
+		logger.Debug().Println("Script Controller: EOF")
 		return nil, io.EOF
 	}
+	logger.Error().Println("Script Controller: Reading from closed controller.")
 	return nil, fmt.Errorf("unexpected controller usage")
 }
 
@@ -82,6 +86,7 @@ func (cc *ScriptController) Write(b []byte) (n int, err error) {
 
 // Close terminates connection
 func (cc *ScriptController) Close() error {
+	logger.Info().Println("Script Controller closing.")
 	close(cc.stop)
 	return nil
 }
