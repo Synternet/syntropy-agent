d165d36f1eeaf3e87d5662a40b88b6320418b457
Antanas Gadeikis <antanas@syntropynet.com>
2021-09-10 11:48:20 +0300

    Minimal route entries unit test.

---
+++ agent/router/entry_test.go
@@ -0,0 +1,37 @@
+package router
+
+import "testing"
+
+func TestEntry(t *testing.T) {
+	rl := routeList{}
+
+	if rl.Count() != 0 {
+		t.Error("Invalid route list initialise")
+	}
+
+	// Add few routes and test
+	rl.Add(&routeEntry{
+		ifname:  "eth0",
+		gateway: "1.1.1.1",
+		id:      111,
+	})
+	rl.Add(&routeEntry{
+		ifname:  "eth1",
+		gateway: "2.2.2.2",
+		id:      222,
+	})
+	if rl.Count() != 2 {
+		t.Error("Invalid route list cout")
+	}
+
+	// Add dupplicate entry
+	rl.Add(&routeEntry{
+		ifname:  "eth1",
+		gateway: "2.2.2.2",
+		id:      222,
+	})
+	if rl.Count() != 2 {
+		t.Error("Dupplicate route added")
+	}
+
+}
