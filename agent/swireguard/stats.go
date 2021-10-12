7495eb5332afaf846381414a254d074077e8bbdb
Antanas Gadeikis <antanas@syntropynet.com>
2021-10-12 17:43:14 +0300

    Peers statistics bugfixes

--- agent/swireguard/stats.go
+++ agent/swireguard/stats.go
@@ -14,28 +14,30 @@ type PeerStats struct {
 }
 
 func (wg *Wireguard) PeerStatsUpdate() {
-	for _, sdev := range wg.devices {
-		wdev, err := wg.wgc.Device(sdev.IfName)
+	for _, agentDev := range wg.devices {
+		osDev, err := wg.wgc.Device(agentDev.IfName)
 		if err != nil {
 			continue
 		}
 
-		for _, peer1 := range wdev.Peers {
-			for _, peer2 := range sdev.peers {
-				if peer2.PublicKey == peer1.PublicKey.String() {
-					peer2.Stats.LastHandshake = peer1.LastHandshakeTime
-					prevTx := peer2.Stats.TxBytes
-					prevRx := peer2.Stats.RxBytes
-					peer2.Stats.TxBytes = peer2.Stats.TxBytes + peer1.TransmitBytes
-					peer2.Stats.RxBytes = peer2.Stats.RxBytes + peer1.ReceiveBytes
-					if !peer2.Stats.timestamp.IsZero() {
-						diff := float32(time.Since(peer2.Stats.timestamp) / time.Second)
+		for _, osPeer := range osDev.Peers {
+			for _, agentPeer := range agentDev.peers {
+				if agentPeer.PublicKey == osPeer.PublicKey.String() {
+					// Calculate statistics first
+					if !agentPeer.Stats.timestamp.IsZero() {
+						diff := float32(time.Since(agentPeer.Stats.timestamp) / time.Second)
 						if diff > 0 {
-							peer2.Stats.TxSpeedMbps = float32(peer2.Stats.TxBytes-prevTx) / diff / 1000000
-							peer2.Stats.RxSpeedMbps = float32(peer2.Stats.RxBytes-prevRx) / diff / 1000000
+							agentPeer.Stats.TxSpeedMbps = float32(osPeer.TransmitBytes-agentPeer.Stats.TxBytes) / diff / 1000000
+							agentPeer.Stats.RxSpeedMbps = float32(osPeer.ReceiveBytes-agentPeer.Stats.RxBytes) / diff / 1000000
 						}
 					}
-					peer2.Stats.timestamp = time.Now()
+					// Then update Tx/Rx bytes for the next time
+					agentPeer.Stats.TxBytes = osPeer.TransmitBytes
+					agentPeer.Stats.RxBytes = osPeer.ReceiveBytes
+
+					// timestamp, handshake, etc
+					agentPeer.Stats.timestamp = time.Now()
+					agentPeer.Stats.LastHandshake = osPeer.LastHandshakeTime
 					// skip to next peer on external loop
 					break
 				}
