924062d8a15f6255e47ecd337c59db6ecec734a6
Antanas Gadeikis <antanas@syntropynet.com>
2021-10-21 15:20:15 +0300

    Simplify errors handling. Get rid of variadic functions in netcfg.Route*

--- pkg/netcfg/route.go
+++ pkg/netcfg/route.go
@@ -7,78 +7,75 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-func RouteAdd(ifname string, gw string, ips ...string) error {
+func RouteAdd(ifname string, gw string, ip string) error {
 	iface, err := netlink.LinkByName(ifname)
 	if err != nil {
 		return fmt.Errorf("failed to lookup interface %s", ifname)
 	}
 	gateway := net.ParseIP(gw)
 
-	for _, ip := range ips {
-		route := netlink.Route{
-			LinkIndex: iface.Attrs().Index,
-			Gw:        gateway,
-		}
-
-		// I need only network address here, no need to "patch" parseCidr's result
-		_, route.Dst, err = net.ParseCIDR(ip)
-		if err != nil {
-			return fmt.Errorf("%s while parsing %s", err.Error(), ip)
-		}
-		err = netlink.RouteAdd(&route)
-		if err != nil {
-			return fmt.Errorf("route %s via %s: %s", ip, gw, err.Error())
-		}
+	route := netlink.Route{
+		LinkIndex: iface.Attrs().Index,
+		Gw:        gateway,
 	}
+
+	// I need only network address here, no need to "patch" parseCidr's result
+	_, route.Dst, err = net.ParseCIDR(ip)
+	if err != nil {
+		return fmt.Errorf("%s while parsing %s", err.Error(), ip)
+	}
+	err = netlink.RouteAdd(&route)
+	if err != nil {
+		return fmt.Errorf("route %s via %s: %s", ip, gw, err.Error())
+	}
+
 	return nil
 }
 
-func RouteDel(ifname string, ips ...string) error {
+func RouteDel(ifname string, ip string) error {
 	iface, err := netlink.LinkByName(ifname)
 	if err != nil {
 		return fmt.Errorf("failed to lookup interface %v", ifname)
 	}
 
-	for _, ip := range ips {
-		routes, err := netlink.RouteList(iface, 0)
-		if err != nil {
-			return err
-		}
-		for _, r := range routes {
-			if r.Dst != nil && r.Dst.String() == ip {
-				err = netlink.RouteDel(&r)
-				if err != nil {
-					return fmt.Errorf("route %s del: %s", ip, err.Error())
-				}
+	routes, err := netlink.RouteList(iface, 0)
+	if err != nil {
+		return err
+	}
+	for _, r := range routes {
+		if r.Dst != nil && r.Dst.String() == ip {
+			err = netlink.RouteDel(&r)
+			if err != nil {
+				return fmt.Errorf("route %s del: %s", ip, err.Error())
 			}
 		}
 	}
+
 	return nil
 }
 
-func RouteReplace(ifname string, gw string, ips ...string) error {
+func RouteReplace(ifname string, gw string, ip string) error {
 	iface, err := netlink.LinkByName(ifname)
 	if err != nil {
 		return fmt.Errorf("failed to lookup interface %v", ifname)
 	}
 	gateway := net.ParseIP(gw)
 
-	for _, ip := range ips {
-		route := netlink.Route{
-			LinkIndex: iface.Attrs().Index,
-			Gw:        gateway,
-		}
-
-		// I need only network address here, no need to "patch" parseCidr's result
-		_, route.Dst, err = net.ParseCIDR(ip)
-		if err != nil {
-			return fmt.Errorf("%s while parsing %s", err.Error(), ip)
-		}
-		err = netlink.RouteReplace(&route)
-		if err != nil {
-			return fmt.Errorf("route replace %s via %s: %s", ip, gw, err.Error())
-		}
+	route := netlink.Route{
+		LinkIndex: iface.Attrs().Index,
+		Gw:        gateway,
 	}
+
+	// I need only network address here, no need to "patch" parseCidr's result
+	_, route.Dst, err = net.ParseCIDR(ip)
+	if err != nil {
+		return fmt.Errorf("%s while parsing %s", err.Error(), ip)
+	}
+	err = netlink.RouteReplace(&route)
+	if err != nil {
+		return fmt.Errorf("route replace %s via %s: %s", ip, gw, err.Error())
+	}
+
 	return nil
 }
 
