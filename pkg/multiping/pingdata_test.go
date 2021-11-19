f8b80fd7a51a22a7abd9fb021fa0657df4b6ba03
Antanas Gadeikis <antanas@syntropynet.com>
2021-11-19 14:56:42 +0200

    Fix and extend pingdata test

--- pkg/multiping/pingdata_test.go
+++ pkg/multiping/pingdata_test.go
@@ -16,14 +16,23 @@ func TestPingData(t *testing.T) {
 	data.Add("127.0.0.1")
 	data.Add("127.0.0.1")
 	data.Add("127.0.0.1")
-	if data.Count() != 0 {
+	if data.Count() != 1 {
 		t.Errorf("Duplicate entries check failed")
 	}
 
-	for i := 2; i < maxCount; i++ {
+	for i := 2; i <= maxCount; i++ {
 		data.Add(fmt.Sprintf("127.0.0.%d", i))
 	}
 	if data.Count() != maxCount {
 		t.Errorf("Total count test failed")
+	}
+
+	data.Reset()
+	if data.Count() != maxCount {
+		t.Errorf("Data reset test failed")
+	}
+	data.Flush()
+	if data.Count() != 0 {
+		t.Errorf("Data flush test failed")
 	}
 }
