bda5d46aac4272ec1627fc9ec995b38e79c94c1d
drgkaleda <antanas@syntropynet.com>
2021-12-02 15:23:31 +0200

    Some cleanup in WG_CONF

---
+++ agent/wgconf/message.go
@@ -0,0 +1,72 @@
+package wgconf
+
+import (
+	"strings"
+
+	"github.com/SyntropyNet/syntropy-agent/agent/common"
+	"github.com/SyntropyNet/syntropy-agent/agent/swireguard"
+	"github.com/SyntropyNet/syntropy-agent/internal/env"
+)
+
+// This struct is not used in Linux agent
+// Is is intendended only for desktop (MacOS and Windows) application
+type allowedIPsInfoEntry struct {
+	ServiceName string `json:"agent_service_name,omitempty"`
+	TcpPorts    []int  `json:"agent_service_tcp_ports,omitempty"`
+	UdpPorts    []int  `json:"agent_service_udp_ports,omitempty"`
+	SubnetIP    string `json:"agent_service_subnet_ip,omitempty"`
+}
+
+type wgConfEntry struct {
+	Function string `json:"fn"`
+	Args     struct {
+		// Interface configuration
+		IfName string `json:"ifname,omitempty"`
+		IP     string `json:"internal_ip,omitempty"`
+		Port   int    `json:"listen_port,omitempty"`
+		// Peer configuration
+		PublicKey    string   `json:"public_key,omitempty"`
+		AllowedIPs   []string `json:"allowed_ips,omitempty"`
+		GatewayIPv4  string   `json:"gw_ipv4,omitempty"`
+		EndpointIPv4 string   `json:"endpoint_ipv4,omitempty"`
+		EndpointPort int      `json:"endpoint_port,omitempty"`
+	}
+	Metadata struct {
+		// Interface configuration
+		NetworkID int `json:"network_id,omitempty"`
+		// Peer configuration
+		DeviceID         string                `json:"device_id,omitempty"`
+		DeviceName       string                `json:"device_name,omitempty"`
+		DevicePublicIPv4 string                `json:"device_public_ipv4,omitempty"`
+		ConnectionID     int                   `json:"connection_id,omitempty"`
+		GroupID          int                   `json:"connection_group_id,omitempty"`
+		AgentID          int                   `json:"agent_id,omitempty"`
+		AllowedIPsInfo   []allowedIPsInfoEntry `json:"allowed_ips_info,omitempty"`
+	}
+}
+
+func (e *wgConfEntry) asPeerInfo() *swireguard.PeerInfo {
+	var ifname string
+	if strings.HasPrefix(e.Args.IfName, env.InterfaceNamePrefix) {
+		ifname = e.Args.IfName
+	} else {
+		ifname = env.InterfaceNamePrefix + e.Args.IfName
+	}
+
+	return &swireguard.PeerInfo{
+		IfName:       ifname,
+		IP:           e.Args.EndpointIPv4,
+		PublicKey:    e.Args.PublicKey,
+		ConnectionID: e.Metadata.ConnectionID,
+		GroupID:      e.Metadata.GroupID,
+		AgentID:      e.Metadata.AgentID,
+		Port:         e.Args.EndpointPort,
+		Gateway:      e.Args.GatewayIPv4,
+		AllowedIPs:   e.Args.AllowedIPs,
+	}
+}
+
+type wgConfMsg struct {
+	common.MessageHeader
+	Data []wgConfEntry `json:"data"`
+}
--- agent/wgconf/wg_conf.go
+++ agent/wgconf/wg_conf.go
@@ -4,12 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"strings"
 
 	"github.com/SyntropyNet/syntropy-agent/agent/common"
 	"github.com/SyntropyNet/syntropy-agent/agent/mole"
-	"github.com/SyntropyNet/syntropy-agent/agent/swireguard"
-	"github.com/SyntropyNet/syntropy-agent/internal/env"
 	"github.com/SyntropyNet/syntropy-agent/internal/logger"
 )
 
@@ -21,111 +18,6 @@ const (
 type wgConf struct {
 	writer io.Writer
 	mole   *mole.Mole
-}
-
-// This struct is not used in Linux agent
-// Is is intendended only for desktop (MacOS and Windows) application
-type allowedIPsInfoEntry struct {
-	ServiceName string `json:"agent_service_name,omitempty"`
-	TcpPorts    []int  `json:"agent_service_tcp_ports,omitempty"`
-	UdpPorts    []int  `json:"agent_service_udp_ports,omitempty"`
-	SubnetIP    string `json:"agent_service_subnet_ip,omitempty"`
-}
-
-type wgConfEntry struct {
-	Function string `json:"fn"`
-	Args     struct {
-		// Interface configuration
-		IfName string `json:"ifname,omitempty"`
-		IP     string `json:"internal_ip,omitempty"`
-		Port   int    `json:"listen_port,omitempty"`
-		// Peer configuration
-		PublicKey    string   `json:"public_key,omitempty"`
-		AllowedIPs   []string `json:"allowed_ips,omitempty"`
-		GatewayIPv4  string   `json:"gw_ipv4,omitempty"`
-		EndpointIPv4 string   `json:"endpoint_ipv4,omitempty"`
-		EndpointPort int      `json:"endpoint_port,omitempty"`
-	}
-	Metadata struct {
-		// Interface configuration
-		NetworkID int `json:"network_id,omitempty"`
-		// Peer configuration
-		DeviceID         string                `json:"device_id,omitempty"`
-		DeviceName       string                `json:"device_name,omitempty"`
-		DevicePublicIPv4 string                `json:"device_public_ipv4,omitempty"`
-		ConnectionID     int                   `json:"connection_id,omitempty"`
-		GroupID          int                   `json:"connection_group_id,omitempty"`
-		AgentID          int                   `json:"agent_id,omitempty"`
-		AllowedIPsInfo   []allowedIPsInfoEntry `json:"allowed_ips_info,omitempty"`
-	}
-}
-
-func (e *wgConfEntry) asPeerInfo() *swireguard.PeerInfo {
-	var ifname string
-	if strings.HasPrefix(e.Args.IfName, env.InterfaceNamePrefix) {
-		ifname = e.Args.IfName
-	} else {
-		ifname = env.InterfaceNamePrefix + e.Args.IfName
-	}
-
-	return &swireguard.PeerInfo{
-		IfName:       ifname,
-		IP:           e.Args.EndpointIPv4,
-		PublicKey:    e.Args.PublicKey,
-		ConnectionID: e.Metadata.ConnectionID,
-		GroupID:      e.Metadata.GroupID,
-		AgentID:      e.Metadata.AgentID,
-		Port:         e.Args.EndpointPort,
-		Gateway:      e.Args.GatewayIPv4,
-		AllowedIPs:   e.Args.AllowedIPs,
-	}
-}
-
-func (e *wgConfEntry) asInterfaceInfo() *swireguard.InterfaceInfo {
-	var ifname string
-	if strings.HasPrefix(e.Args.IfName, env.InterfaceNamePrefix) {
-		ifname = e.Args.IfName
-	} else {
-		ifname = env.InterfaceNamePrefix + e.Args.IfName
-	}
-
-	return &swireguard.InterfaceInfo{
-		IfName:    ifname,
-		IP:        e.Args.IP,
-		PublicKey: e.Args.PublicKey,
-		Port:      e.Args.Port,
-	}
-}
-
-type wgConfMsg struct {
-	common.MessageHeader
-	Data []wgConfEntry `json:"data"`
-}
-
-func (msg *wgConfMsg) AddPeerCmd(cmd string, pi *swireguard.PeerInfo) {
-	e := wgConfEntry{
-		Function: cmd,
-	}
-	e.Args.IfName = pi.IfName
-	e.Args.EndpointIPv4 = pi.IP
-	e.Args.PublicKey = pi.PublicKey
-	e.Args.EndpointPort = pi.Port
-	e.Args.GatewayIPv4 = pi.Gateway
-	e.Args.AllowedIPs = pi.AllowedIPs
-
-	msg.Data = append(msg.Data, e)
-}
-
-func (msg *wgConfMsg) AddInterfaceCmd(cmd string, ii *swireguard.InterfaceInfo) {
-	e := wgConfEntry{
-		Function: cmd,
-	}
-	e.Args.IfName = ii.IfName
-	e.Args.IP = ii.IP
-	e.Args.PublicKey = ii.PublicKey
-	e.Args.Port = ii.Port
-
-	msg.Data = append(msg.Data, e)
 }
 
 func New(w io.Writer, m *mole.Mole) common.Command {
