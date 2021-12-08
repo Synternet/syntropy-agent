5f18faea05abd285e85ad32ed60b84d41553246f
drgkaleda <antanas@syntropynet.com>
2021-12-08 12:20:29 +0200

    Split CONFIG_INFO message to separate files (request vs response)

--- agent/configinfo/req_msg.go
+++ agent/configinfo/req_msg.go
@@ -114,38 +114,3 @@ type configInfoMsg struct {
 		Subnetworks []configInfoSubnetworksEntry `json:"subnetworks,omitempty"`
 	} `json:"data"`
 }
-
-type updateAgentConfigEntry struct {
-	Function string `json:"fn"`
-	Data     struct {
-		IfName    string `json:"ifname"`
-		PublicKey string `json:"public_key"`
-		IP        string `json:"internal_ip"`
-		Port      int    `json:"listen_port"`
-	} `json:"data"`
-}
-
-type updateAgentConfigMsg struct {
-	common.MessageHeader
-	Data []updateAgentConfigEntry `json:"data"`
-}
-
-func (msg *updateAgentConfigMsg) AddInterface(data *swireguard.InterfaceInfo) {
-	e := updateAgentConfigEntry{Function: "create_interface"}
-	e.Data.IfName = data.IfName
-	e.Data.IP = data.IP
-	e.Data.PublicKey = data.PublicKey
-	e.Data.Port = data.Port
-
-	msg.Data = append(msg.Data, e)
-}
-
-func (msg *updateAgentConfigMsg) AddPeer(data *swireguard.PeerInfo) {
-	e := updateAgentConfigEntry{Function: "add_peer"}
-	e.Data.IfName = data.IfName
-	e.Data.IP = data.IP
-	e.Data.PublicKey = data.PublicKey
-	e.Data.Port = data.Port
-
-	msg.Data = append(msg.Data, e)
-}
