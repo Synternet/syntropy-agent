228c4e1a177c0cc4b0fd9fc6ac4a64a1c0d7fdc0
drgkaleda <antanas@syntropynet.com>
2022-02-09 15:18:22 +0200

    Broken unit test fix

--- agent/router/peermon/peermon_test.go
+++ agent/router/peermon/peermon_test.go
@@ -10,13 +10,13 @@ func TestPeerMonitor(t *testing.T) {
 	count := 24
 	pm := New(uint(count))
 
-	addNode := func(gw, ip string) {
-		pm.AddNode("ifname", "PublicKey", gw, ip, 0)
+	addNode := func(ip string) {
+		pm.AddNode("ifname", "PublicKey", ip, 0)
 	}
 
 	fillStats := func(endpoint string, latency, loss float32) {
 		for _, peer := range pm.peerList {
-			if peer.endpoint == endpoint {
+			if peer.ip == endpoint {
 				for i := 0; i < count; i++ {
 					peer.Add(latency, loss)
 				}
@@ -25,10 +25,10 @@ func TestPeerMonitor(t *testing.T) {
 
 	}
 
-	addNode("1.1.1.1", "1.1.1.2")
-	addNode("2.2.2.1", "2.2.2.2")
-	addNode("3.3.3.1", "3.3.3.2")
-	addNode("4.4.4.1", "4.4.4.2")
+	addNode("1.1.1.2")
+	addNode("2.2.2.2")
+	addNode("3.3.3.2")
+	addNode("4.4.4.2")
 	pm.lastBest = 0
 
 	// Lower loss is always must
@@ -37,7 +37,7 @@ func TestPeerMonitor(t *testing.T) {
 	fillStats("3.3.3.2", 500, 0)
 	fillStats("4.4.4.2", 105, 0.3)
 	best := pm.BestPath()
-	if best != "3.3.3.1" {
+	if best != "3.3.3.2" {
 		t.Errorf("Lowest loss test failed %s", best)
 	}
 
@@ -49,7 +49,7 @@ func TestPeerMonitor(t *testing.T) {
 	fillStats("3.3.3.2", 250, 0)
 	fillStats("4.4.4.2", 95, 0)
 	best = pm.BestPath()
-	if best != "4.4.4.1" {
+	if best != "4.4.4.2" {
 		t.Errorf("Test without threshold %s", best)
 	}
 
@@ -57,14 +57,14 @@ func TestPeerMonitor(t *testing.T) {
 	config.SetRerouteThresholds(10, 1.05)
 	pm.lastBest = 0
 	best = pm.BestPath()
-	if best != "1.1.1.1" {
+	if best != "1.1.1.2" {
 		t.Errorf("Test with too big threshold %s", best)
 	}
 
 	config.SetRerouteThresholds(5, 1.05)
 	pm.lastBest = 0
 	best = pm.BestPath()
-	if best != "4.4.4.1" {
+	if best != "4.4.4.2" {
 		t.Errorf("Test with correct threshold %s", best)
 	}
 
@@ -72,7 +72,7 @@ func TestPeerMonitor(t *testing.T) {
 	pm.lastBest = 0
 	pm.peerList[3].Add(0, 0)
 	best = pm.BestPath()
-	if best != "1.1.1.1" {
+	if best != "1.1.1.2" {
 		t.Errorf("Test with incomplete statistics %s", best)
 	}
 }
