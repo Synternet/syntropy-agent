3f36e2c3704576e5e79cd9b1bbe95ea8ab39f900
drgkaleda <antanas@syntropynet.com>
2022-02-24 12:16:14 +0200

    PubIP package do not print about IP change

--- pkg/pubip/pubip.go
+++ pkg/pubip/pubip.go
@@ -90,10 +90,6 @@ func GetPublicIp() net.IP {
 
 		// Lets hope we have some result and parse them
 		if err == nil {
-			if !ip.Equal(publicIP.cache.ip) && publicIP.cache.ip != nil {
-				logger.Info().Println(pkgName, "Public IP Change",
-					publicIP.cache.ip.String(), "-->", ip.String())
-			}
 			publicIP.cache.ip = ip
 			publicIP.cache.updated = time.Now()
 		} else {
