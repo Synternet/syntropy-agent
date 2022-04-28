e68ef608def5d7fcfbed274c1ac1cf57f55dd5c3
drgkaleda <antanas@syntropynet.com>
2022-04-28 13:25:12 +0300

    Unit test fix

--- agent/router/peermon/peermon_test.go
+++ agent/router/peermon/peermon_test.go
@@ -37,8 +37,8 @@ func TestPeerMonitor(t *testing.T) {
 	fillStats("3.3.3.2", 500, 0)
 	fillStats("4.4.4.2", 105, 0.3)
 	best := pm.BestPath()
-	if best != "3.3.3.2" {
-		t.Errorf("Lowest loss test failed %s", best)
+	if best.IP != "3.3.3.2" {
+		t.Errorf("Lowest loss test failed %s", best.IP)
 	}
 
 	// Test without thresholds
@@ -49,30 +49,30 @@ func TestPeerMonitor(t *testing.T) {
 	fillStats("3.3.3.2", 250, 0)
 	fillStats("4.4.4.2", 95, 0)
 	best = pm.BestPath()
-	if best != "4.4.4.2" {
-		t.Errorf("Test without threshold %s", best)
+	if best.IP != "4.4.4.2" {
+		t.Errorf("Test without threshold %s", best.IP)
 	}
 
 	// Set thresholds and test
 	config.SetRerouteThresholds(10, 1.05)
 	pm.lastBest = 0
 	best = pm.BestPath()
-	if best != "1.1.1.2" {
-		t.Errorf("Test with too big threshold %s", best)
+	if best.IP != "1.1.1.2" {
+		t.Errorf("Test with too big threshold %s", best.IP)
 	}
 
 	config.SetRerouteThresholds(5, 1.05)
 	pm.lastBest = 0
 	best = pm.BestPath()
-	if best != "4.4.4.2" {
-		t.Errorf("Test with correct threshold %s", best)
+	if best.IP != "4.4.4.2" {
+		t.Errorf("Test with correct threshold %s", best.IP)
 	}
 
 	// test incomplete statistics
 	pm.lastBest = 0
 	pm.peerList[3].Add(0, 0)
 	best = pm.BestPath()
-	if best != "1.1.1.2" {
-		t.Errorf("Test with incomplete statistics %s", best)
+	if best.IP != "1.1.1.2" {
+		t.Errorf("Test with incomplete statistics %s", best.IP)
 	}
 }
