Synternet/syntropy-agent

commit 22d3f14543cd4092c265919a654e2474bee9ff05

Some refactor on controller. Want to reduce time between connect to cloud And first read. This is done just in case other part of initialisation does unexpected delays (like iptables -L was) And this results in WebSockets ping-pong timeout.

cmd/main.go
@@ -107,6 +107,8 @@ func main() {
 		return
 	}
 
+	logger.SetupGlobalLoger(nil, config.GetDebugLevel(), os.Stdout)
+
 	requireRoot()
 	requireProcFilesystemWritable()
 
@@ -144,6 +146,12 @@ func main() {
 		checkKernelVersion()
 		exitCode = -int(unix.ENOMEM)
 		return
+	}
+
+	if config.GetControllerType() == config.ControllerSaas {
+		// Config loggers early - to get more info logged
+		// NOTE: Setup remote logger only for saas controller
+		logger.SetupGlobalLoger(syntropyNetAgent.Writer(), config.GetDebugLevel(), os.Stdout)
 	}
 
 	logger.Info().Println(fullAppName, execName, config.GetFullVersion(), "started.")

agent/agent.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
 
 	"github.com/SyntropyNet/syntropy-agent/agent/autoping"
 	"github.com/SyntropyNet/syntropy-agent/agent/common"
@@ -59,14 +58,10 @@ func New(contype int) (*Agent, error) {
 	var err error
 	var controller controller.Controller
 
-	logger.SetupGlobalLoger(nil, config.GetDebugLevel(), os.Stdout)
 	switch contype {
 	case config.ControllerSaas:
 		controller, err = saas.New()
 		if controller != nil {
-			// Config loggers early - to get more info logged
-			// Setup remote logger only for saas controller
-			logger.SetupGlobalLoger(controller, config.GetDebugLevel(), os.Stdout)
 		}
 	case config.ControllerScript:
 		controller, err = script.New()
@@ -158,7 +153,7 @@ func New(contype int) (*Agent, error) {
 		shellcmd.New("routes", "route", "-n"),
 		autoping))
 
-	return agent, nil
+	return agent, agent.controller.Open()
 }
 
 // Starts worker services and executes the message loop.
@@ -212,4 +207,9 @@ func (agent *Agent) Close() error {
 	logger.Debug().Println(pkgName, "Agent close done")
 
 	return nil
+}
+
+// Writer returns pointer to controller, as a io.Writer interface
+func (agent *Agent) Writer() io.Writer {
+	return agent.controller
 }

controller/blockchain/blockchain.go
@@ -140,7 +140,11 @@ func New() (controller.Controller, error) {
 		return nil, err
 	}
 
-	return &bc, bc.connect()
+	return &bc, nil
+}
+
+func (bc *BlockchainController) Open() error {
+	return bc.connect()
 }
 
 func (bc *BlockchainController) connect() error {

controller/controller.go
@@ -5,6 +5,11 @@ import (
 )
 
 type Controller interface {
+	// Open function does additional initialisation to controller (for those that need it)
+	// E.g. its nice to have late connecting to SaaS controller,
+	// because you cannot do long delays in WebSocket connection,
+	// because it results in WebSockets timeout
+	Open() error
 	// The primary idea was to use Reader interface here
 	// But the reader may read a partial message and this will complicate agent main loop
 	// and agent would be responsible for receiving and joining all message parts.

controller/saas/saas.go
@@ -24,7 +24,8 @@ const pkgName = "Saas Controller. "
 const reconnectDelay = 10000 // 10 seconds (in milliseconds)
 const (
 	// State machine constants
-	stopped = iota
+	initialised = iota
+	stopped
 	connecting
 	running
 )
@@ -78,20 +79,25 @@ func New() (controller.Controller, error) {
 		token:   config.GetAgentToken(),
 		version: config.GetVersion(),
 	}
-	cc.SetState(stopped)
+	cc.SetState(initialised)
 
 	// Create new local logger for controller events
 	// I am using configured DebugLevel here, but actually
 	// only Errors and Warnings should be logged on this logger.
 	cc.log = logger.New(nil, config.GetDebugLevel(), os.Stdout)
-	cc.log.Info().Println(pkgName, "Connecting...")
-
-	err = cc.connect()
-	if err != nil {
-		return nil, err
-	}
 
 	return &cc, nil
+}
+
+func (cc *CloudController) Open() error {
+	state := cc.GetState()
+	if state != initialised {
+		return fmt.Errorf("unexpected controller state %d", state)
+	}
+
+	cc.log.Info().Println(pkgName, "Connecting...")
+
+	return cc.connect()
 }
 
 func (cc *CloudController) connect() (err error) {

controller/script/script.go
@@ -52,6 +52,10 @@ func New() (controller.Controller, error) {
 	return &cc, nil
 }
 
+func (cc *ScriptController) Open() error {
+	return nil
+}
+
 func delay(cmd string) {
 	arr := strings.Fields(cmd)
 	seconds, err := strconv.Atoi(arr[1])

commit 6fd03318dbf027c33aa1736fba989b5e9a3172b6

Setup logger after config parsing, Fix regression introduced in 22d3f14543cd4092c265919a654e2474bee9ff05

cmd/main.go
@@ -107,8 +107,6 @@ func main() {
 		return
 	}
 
-	logger.SetupGlobalLoger(nil, config.GetDebugLevel(), os.Stdout)
-
 	requireRoot()
 	requireProcFilesystemWritable()
 
@@ -139,6 +137,8 @@ func main() {
 	// Parse configuration environment variables
 	config.Init()
 	defer config.Close()
+
+	logger.SetupGlobalLoger(nil, config.GetDebugLevel(), os.Stdout)
 
 	syntropyNetAgent, err := agent.New(config.GetControllerType())
 	if err != nil {